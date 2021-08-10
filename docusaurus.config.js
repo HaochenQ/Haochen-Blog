f903c14fa96b088684d14f593ae384d50873c478
HaochenQ <haochenqi1997@gmail.com>
2021-08-10 21:53:04 +0800

    label for languages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -120,5 +120,13 @@ module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "zh-cn"],
+    localeConfigs: {
+      en: {
+        label: "English",
+      },
+      "zh-cn": {
+        label: "中文",
+      },
+    },
   },
 };
