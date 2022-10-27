b943e1a29c5499e54e1de726e7750de9212da28e
Haochen(Howard) Qi <44130343+HaochenQ@users.noreply.github.com>
2022-10-27 17:19:10 +0800

    update the url for the cozyspace project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ const Projects = () => {
         "Cozy Space is an online furniture store for you to design your dream home. You can easily find your favorite piece from our various morden furnitures by using the filter function. It is built with React, React hooks(useReduce and useContext) are utlized to manage the state.",
       title: "Cozy Space",
       github: "https://github.com/HaochenQ/React-Furniture-Store",
-      url: "https://cozyspace.netlify.app/",
+      url: "https://cozyspace.haochenqi.com/",
       image: "img/furniture-store.png",
       index: 0,
     },
